df621d7fc8643c7174cacbbb972fc791971ab181
Chris Rink <chrismrink@gmail.com>
2013-03-13 13:20:22 -0700

    Sync File for pushing files to previous directory.

---
+++ sync.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+#Copied from Paul Irish's dotfiles installer
+cd "$(dirname "$0")"
+git pull
+function doIt() {
+	rsync --exclude ".git/" --exclude ".DS_Store" --exclude "sync.sh" --exclude "README.md" -av . ~
+}
+if [ "$1" == "--force" -o "$1" == "-f" ]; then
+	doIt
+else
+	read -p "This may overwrite existing files. Are you sure? (y/n) " -n 1
+	echo
+	if [[ $REPLY =~ ^[Yy]$ ]]; then
+		doIt
+	fi
+fi
+unset doIt
